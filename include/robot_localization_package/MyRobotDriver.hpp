1797dcad68e6607ca3c63ba0661bc476ca623c12
Joao <Joao@x.com>
2025-03-02 12:14:07 +0000

    Robot supervisor ground truth TF broadcast

---
+++ include/robot_localization_package/MyRobotDriver.hpp
@@ -0,0 +1,30 @@
+#ifndef WEBOTS_ROS2_PLUGIN_EXAMPLE_HPP
+#define WEBOTS_ROS2_PLUGIN_EXAMPLE_HPP
+
+#include "rclcpp/macros.hpp"
+#include "webots_ros2_driver/PluginInterface.hpp"
+#include "webots_ros2_driver/WebotsNode.hpp"
+#include "tf2_ros/transform_broadcaster.h"
+#include "geometry_msgs/msg/twist.hpp"
+#include "rclcpp/rclcpp.hpp"
+
+namespace my_robot_driver {
+class MyRobotDriver : public webots_ros2_driver::PluginInterface {
+public:
+  void step() override;
+  void init(webots_ros2_driver::WebotsNode *node,
+            std::unordered_map<std::string, std::string> &parameters) override;
+
+private:
+
+  rclcpp::Subscription<geometry_msgs::msg::Twist>::SharedPtr
+      cmd_vel_subscription_;
+      
+  geometry_msgs::msg::Twist cmd_vel_msg;
+
+  WbDeviceTag right_motor;
+  WbDeviceTag left_motor;
+  std::shared_ptr<tf2_ros::TransformBroadcaster> tf_broadcaster_;
+};
+} // namespace my_robot_driver
+#endif
---
+++ src/MyRobotDriver.cpp
@@ -0,0 +1,94 @@
+#include "robot_localization_package/MyRobotDriver.hpp"
+
+#include "rclcpp/rclcpp.hpp"
+#include <cstdio>
+#include <functional>
+#include <webots/motor.h>
+#include <webots/robot.h>
+#include <webots/supervisor.h>
+#include <tf2/LinearMath/Quaternion.h>
+#include <tf2/LinearMath/Matrix3x3.h>
+#include "tf2_ros/transform_broadcaster.h"
+#include "geometry_msgs/msg/transform_stamped.hpp"
+
+#define HALF_DISTANCE_BETWEEN_WHEELS 0.045
+#define WHEEL_RADIUS 0.025
+
+
+namespace my_robot_driver {
+
+WbNodeRef robot_node = wb_supervisor_node_get_from_def("MY_ROBOT");
+
+void MyRobotDriver::init(
+    webots_ros2_driver::WebotsNode *node,
+    std::unordered_map<std::string, std::string> &parameters) {
+
+  right_motor = wb_robot_get_device("right wheel motor");
+  left_motor = wb_robot_get_device("left wheel motor");
+
+
+  wb_motor_set_position(left_motor, INFINITY);
+  wb_motor_set_velocity(left_motor, 0.0);
+
+  wb_motor_set_position(right_motor, INFINITY);
+  wb_motor_set_velocity(right_motor, 0.0);
+
+  cmd_vel_subscription_ = node->create_subscription<geometry_msgs::msg::Twist>(
+      "/cmd_vel", rclcpp::SensorDataQoS().reliable(),
+      [this](const geometry_msgs::msg::Twist::SharedPtr msg){
+        this->cmd_vel_msg.linear = msg->linear;
+        this->cmd_vel_msg.angular = msg->angular;
+      }
+  );
+
+  tf_broadcaster_ = std::make_shared<tf2_ros::TransformBroadcaster>(node);
+
+}
+
+void MyRobotDriver::step() {
+  auto forward_speed = cmd_vel_msg.linear.x;
+  auto angular_speed = cmd_vel_msg.angular.z;
+
+  auto command_motor_left =
+      (forward_speed - angular_speed * HALF_DISTANCE_BETWEEN_WHEELS) /
+      WHEEL_RADIUS;
+  auto command_motor_right =
+      (forward_speed + angular_speed * HALF_DISTANCE_BETWEEN_WHEELS) /
+      WHEEL_RADIUS;
+
+  wb_motor_set_velocity(left_motor, command_motor_left);
+  wb_motor_set_velocity(right_motor, command_motor_right);
+
+  const double *position = wb_supervisor_node_get_position(robot_node);
+  const double *orientation = wb_supervisor_node_get_orientation(robot_node);
+
+  geometry_msgs::msg::TransformStamped tf;
+
+  tf2::Matrix3x3 mat(orientation[0], orientation[1], orientation[2],
+                     orientation[3], orientation[4], orientation[5],
+                     orientation[6], orientation[7], orientation[8]);
+
+  
+  tf2::Quaternion q;
+  mat.getRotation(q);
+
+  tf.header.stamp = rclcpp::Clock().now();
+  tf.header.frame_id = "odom";
+  tf.child_frame_id = "base_link";
+  tf.transform.rotation.x = q.x();
+  tf.transform.rotation.y = q.y();
+  tf.transform.rotation.z = q.z();
+  tf.transform.rotation.w = q.w();
+
+
+  tf.transform.translation.x = position[0];
+  tf.transform.translation.y = position[1];
+  tf.transform.translation.z = position[2];
+
+  tf_broadcaster_->sendTransform(tf);
+}
+} // namespace my_robot_driver
+
+#include "pluginlib/class_list_macros.hpp"
+PLUGINLIB_EXPORT_CLASS(my_robot_driver::MyRobotDriver,
+                       webots_ros2_driver::PluginInterface)
