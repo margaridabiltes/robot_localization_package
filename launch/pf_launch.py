bc43d5438907cd25e866a53e72db289b14c256e3
Joao <Joao@x.com>
2025-05-22 15:43:37 +0100

    New feature format

--- launch/pf_launch.py
+++ launch/pf_launch.py
@@ -81,7 +81,7 @@ def generate_launch_description():
     )
 
     # Particle filter
-    particle_filter_config_file = "/home/rui/ros_ws/src/robot_localization_package/config/particle_filter_params.yaml"
+    particle_filter_config_file = "/home/joao/ros_ws/src/robot_localization_package/config/particle_filter_params.yaml"
     particle_filter = Node(
         package='robot_localization_package',
         executable='particle_filter',
@@ -158,9 +158,9 @@ def generate_launch_description():
         rviz,
         webots,
         robot_controller,
-        fake_detector,
+        #fake_detector,
         # path_tracker,
-        # corner_detector,
+        corner_detector,
         particle_filter,
         # send_scan,
         # recv_results,
